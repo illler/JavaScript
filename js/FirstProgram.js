8d31ba59c3ffd0d5d17f09e20c480aa9a965a940
Alexandr <0250901@gmail.com>
2023-02-12 23:10:56 +0300

    lesson 19, start coding first console program

--- js/FirstProgram.js
+++ js/FirstProgram.js
@@ -1,22 +1,25 @@
 "use strict";
 
-// alert('Hello')
-
-// const res = confirm('Are you here?');
-// console.log(res)
-
-
-// const ans = +prompt('Вам есть 18?', '18')
-// console.log(ans + 5)
-
-const answers = [];
-// for (let i = 0; i < 3; i++) {
-//     answers[i] = prompt('Ваше имя', '');
-// }
-// console.log(answers)
-
-console.log(typeof (answers))
-
 let s = 5, w = 5;
 console.log(s++);
 console.log(++w);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+
+
+for (let i = 0; i < numberOfFilms; i++) {
+    const nameMovie = prompt('Один из последних просмотренных фильмов?', '');
+    personalMovieDB.movies[nameMovie] = prompt('На сколько оцените его?', '');
+}
+
+
+console.log(personalMovieDB)
