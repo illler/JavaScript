6031d3655405684be5ecac9f190466e88f39b25e
Alexandr <0250901@gmail.com>
2023-02-12 16:37:27 +0300

    first commit

---
+++ js/FirstProgram.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert('Hello')
+
+// const res = confirm('Are you here?');
+// console.log(res)
+
+
+// const ans = +prompt('Вам есть 18?', '18')
+// console.log(ans + 5)
+
+const answers = [];
+// for (let i = 0; i < 3; i++) {
+//     answers[i] = prompt('Ваше имя', '');
+// }
+// console.log(answers)
+
+console.log(typeof (answers))
+
+let s = 5, w = 5;
+console.log(s++);
+console.log(++w);
