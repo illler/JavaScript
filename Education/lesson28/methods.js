e98d6d6f9a59b895448f60ada8f0c6df0ff7eb14
Alexandr <0250901@gmail.com>
2023-02-13 21:58:50 +0300

    String and Number method

---
+++ Education/lesson28/methods.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const str = 'test';
+
+console.log(str);
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('t', 2))
+
+const logg = 'Hello world';
+
+console.log(logg.slice(5, 4))
+
+console.log(logg.substring(1, 4))
+
+const num = 60;
+
+console.log(Math.random()*10)
+
+const test = "12.2px";
+console.log(parseFloat(test) + 1)
+
+
+
