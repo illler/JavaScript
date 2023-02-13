668a210a545f5e7fdc944295f8f510d1d10c60cf
Alexandr <0250901@gmail.com>
2023-02-13 20:19:22 +0300

    Lesson 24, continue coding console app

--- js/FirstProgram.js
+++ js/FirstProgram.js
@@ -4,7 +4,7 @@ let s = 5, w = 5;
 console.log(s++);
 console.log(++w);
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -15,11 +15,25 @@ const personalMovieDB = {
 }
 
 
-
 for (let i = 0; i < numberOfFilms; i++) {
-    const nameMovie = prompt('Один из последних просмотренных фильмов?', '');
-    personalMovieDB.movies[nameMovie] = prompt('На сколько оцените его?', '');
+    const nameMovie = prompt('Один из последних просмотренных фильмов?', ''),
+        rating = prompt('На сколько оцените его?', '');
+    if (nameMovie != null && rating!=null && nameMovie!=='' && nameMovie.length<50) {
+        personalMovieDB.movies[nameMovie] = rating;
+    } else {
+        console.log('error');
+        i--;
+    }
+
 }
+
+if (personalMovieDB.count<10){
+    console.log('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count<30){
+    console.log('Вы классический зритель')
+} else if (personalMovieDB.count>=30){
+    console.log('Вы кономан')
+}else console.log('error')
 
 
 console.log(personalMovieDB)
